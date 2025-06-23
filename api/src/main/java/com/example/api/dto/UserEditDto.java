89a836edd7ca7be676e13bfe1f7064b96691c1d5
shunsuke.sakaya <shunsuke.sakaya@insight-inc.biz>
2025-06-23 11:08:37 +0900

    Evaluationscontrollerにsを付けました

--- api/src/main/java/com/example/api/dto/UserEditDto.java
+++
@@ -1,8 +0,0 @@
-package com.example.api.dto;
-
-public class UserEditDto extends BaseUserDto {
-    private Integer id;
-
-    public Integer getId() { return id; }
-    public void setId(Integer id) { this.id = id; }
-}
